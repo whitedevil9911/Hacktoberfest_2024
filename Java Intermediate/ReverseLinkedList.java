8ac3b2af4d0032836740313f58b97bbc568f2794
HARIOM KUMAR PANDIT <138400713+hari7261@users.noreply.github.com>
2024-10-08 15:48:04 +0530

    Create ReverseLinkedList.java

---
+++ Java Intermediate/ReverseLinkedList.java
@@ -0,0 +1,63 @@
+class Node {
+    int data;
+    Node next;
+    
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+    Node head;
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void reverse() {
+        Node prev = null, current = head, next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    public static void main(String[] args) {
+        ReverseLinkedList list = new ReverseLinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        
+        System.out.println("Original List:");
+        list.display();
+        
+        list.reverse();
+        
+        System.out.println("Reversed List:");
+        list.display();
+    }
+}
