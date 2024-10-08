960ab956d06b96ffcbaf535c0b00dda0cad8c296
HARIOM KUMAR PANDIT <138400713+hari7261@users.noreply.github.com>
2024-10-08 14:29:40 +0530

    Create PalindromeCheck.java

---
+++ Java Basics/PalindromeCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        
+        String reversedStr = new StringBuilder(str).reverse().toString();
+        
+        if (str.equals(reversedStr)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+}
