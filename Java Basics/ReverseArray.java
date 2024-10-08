0515864018b526328f42d7cc60b63c46b0eb8029
HARIOM KUMAR PANDIT <138400713+hari7261@users.noreply.github.com>
2024-10-08 14:34:03 +0530

    Create ReverseArray.java

---
+++ Java Basics/ReverseArray.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        System.out.print("Reversed Array: ");
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
