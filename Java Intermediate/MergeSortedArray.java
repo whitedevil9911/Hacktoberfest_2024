65a93d1289fffa5b587b3095d66981761b50a919
HARIOM KUMAR PANDIT <138400713+hari7261@users.noreply.github.com>
2024-10-08 15:50:31 +0530

    Create MergeSortedArray.java

---
+++ Java Intermediate/MergeSortedArray.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2, 4, 6, 8};
+        
+        int[] mergedArray = mergeArrays(arr1, arr2);
+        System.out.println("Merged Sorted Array: " + Arrays.toString(mergedArray));
+    }
+
+    public static int[] mergeArrays(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                result[k++] = arr1[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+        }
+
+        while (i < arr1.length) {
+            result[k++] = arr1[i++];
+        }
+
+        while (j < arr2.length) {
+            result[k++] = arr2[j++];
+        }
+
+        return result;
+    }
+}
