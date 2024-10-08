839228223e6c549e75153fd25c641b81b0cee428
HARIOM KUMAR PANDIT <138400713+hari7261@users.noreply.github.com>
2024-10-08 14:32:53 +0530

    Create PrimeCheck.java

---
+++ Java Basics/PrimeCheck.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        boolean isPrime = true;
+        if (num <= 1) {
+            isPrime = false;
+        }
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
